f19bd67650ab102701b852a3b848a4d859172c3f
Amminn <vrb.nnn@gmail.com>
2022-01-18 17:05:57 +0100

    Finished Adding large screen styling, added scroll animations, current link

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 const menuToggleIcon = document.getElementById('menu-toggle-icon');
 const navMobile = document.getElementById('nav');
 const headerElement = document.getElementById('header');
-// const menuToggleIcon = document.getElementById('menu-toggle-icon');
+const navLinks = document.querySelectorAll('.nav-mobile .list-link');
 
 // Toggle navigation
 const toggleMenu = () => {
     navMobile.classList.toggle('active')
-    headerElement .classList.toggle('active')
+    headerElement.classList.toggle('active')
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu)
@@ -20,11 +20,60 @@ const headerScrollEvent = () => {
     }
 }
 
-window.addEventListener('scroll', headerScrollEvent)
 //Add/remove header border bottom on scroll
+window.addEventListener('scroll', headerScrollEvent)
+
 
 
 // Add selected link styles to the link clicked and close navigation when clicking the links
+navLinks.forEach(link => link.addEventListener('click', () => {
+    navMobile.classList.remove('active')
+    headerElement.classList.remove('active')
+
+    // select the link with current class
+    let current = document.querySelector('.current')
+
+    // remove the current class from the current element
+    current.classList.remove('current')
+
+    // add current class to the link we did click
+    link.classList.add('current')
+}))
 
 
-// Scroll Reveal
+// Scroll Reveal animation
+
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 2500
+})
+
+sr.reveal(`.image-left, .popular-destionation-data, .plan-trip-data`, {
+    origin: 'left'
+}) 
+
+sr.reveal(`.image-center`, {
+    origin: 'bottom'
+})
+
+sr.reveal(`.image-right, .showcase-data`, {
+    origin: 'right'
+}) 
+
+sr.reveal(`.newsletter-container-inner`, {
+    origin: 'bottom',
+    interval: 250
+}) 
+sr.reveal(`.beach-data-wrapper`, {
+    origin: 'bottom',
+    interval: 250
+}) 
+
+sr.reveal(`.newsletter-container`, {
+    origin: 'top'
+})
+
+sr.reveal(`.footer-container > div, .footer-seperator, .copyright`, {
+    origin: 'bottom',
+    interval: 250
+})
